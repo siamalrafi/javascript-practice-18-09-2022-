3bd8c27a8a579c807bc4ff524706fb451e28a3f6
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-09-18 00:42:17 +0600

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,69 @@
+// show the larger value of 10;
+
+/* const ages = [32, 33, 8, 2, 10, 16, 40];
+
+const result = ages.filter(checkAdult = (ages) => {
+    return ages > 10;
+})
+console.log(result); */
+
+////show the first larger value of 10;
+/* 
+const ages = [3, 10, 18, 20];
+const number = ages.find(array = (agess) => {
+    return agess > 10;
+})
+
+console.log(number); */
+
+// show the index of larger value from 10;
+/* 
+const ages = [3, 10, 18, 20];
+
+const number = ages.findIndex(name = (age)=>{
+    return age > 10;
+})
+console.log(number); */
+
+/* const fruits = ["apple", "orange", "cherry", 'mama'];
+fruits.forEach(myFunction);
+
+function myFunction(currentValue, item, index) {
+    console.log(currentValue, item, index);
+    return (currentValue, item, index)
+} */
+
+/* const numbers = [65, 44, 12, 4];
+numbers.forEach(myFunction)
+
+function myFunction(item, index, arr) {
+    arr[index] = item * 10;
+}
+
+console.log(numbers); */
+
+/* let text = "ABCDEFG";
+const name = Array.from(text);
+console.log(name); */
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const name = fruits.includes("Banana", 0);
+console.log(name); */
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let result =  Array.isArray(fruits);
+console.log(result); */
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+Array.prototype.myUcase = function () {
+    for (let i = 0; i < this.length; i++) {
+        this[i] = this[i].toUpperCase();
+    }
+};
+fruits.myUcase();
+console.log(fruits);
+ */
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.toString();
+console.log(text); */
